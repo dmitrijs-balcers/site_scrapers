9849541355c7b3741a20623642d7795df4cad21a
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2021-11-21 22:58:17 +0200

    Fetch car details per dealership

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import asyncio
 import time
 from itertools import chain, islice, takewhile, repeat
-from typing import List, Iterable, Sequence, TypeVar, Tuple
+from typing import List, Iterable, Sequence, TypeVar, Tuple, Callable
 
 import httpx
 from returns.future import future_safe, FutureResultE
+from returns.io import IOResultE
 from returns.iterables import Fold
 from returns.result import Result, Success
 
@@ -14,6 +15,8 @@ from scrapers.mollerAuto import fetch_moller_auto_list
 from scrapers.inchcape import fetch_inchcape_list
 
 import multiprocessing as mp
+
+from utils.sync_to_async import sync_to_async
 
 
 @future_safe
@@ -40,7 +43,7 @@ async def fetch_cars(urls: Iterable[str]) -> List[FutureResultE[str]]:
     return futures
 
 
-async def aaa(res: Iterable[Car]) -> List[FutureResultE[str]]:
+async def fetch_cars_in_batches(res: Iterable[Car]) -> List[FutureResultE[str]]:
     url_batches = split_every(10, [car.url for car in res])
     car_details: List[FutureResultE[str]] = []
     for url_batch in url_batches:
@@ -49,26 +52,22 @@ async def aaa(res: Iterable[Car]) -> List[FutureResultE[str]]:
     return car_details
 
 
+async def fetch_car_pages(fn: Callable[[], Result[Iterable[Car], str]]) -> List[FutureResultE[str]]:
+    list: Result[Iterable[Car], str] = await sync_to_async(fn)()
+    return await fetch_cars_in_batches(list.value_or([]))
+
+
+async def gather_lists() -> Sequence[IOResultE[List[FutureResultE[str]]]]:
+    return await asyncio.gather(*[
+        fetch_car_pages(fetch_moller_auto_list),
+        fetch_car_pages(fetch_inchcape_list),
+        fetch_car_pages(fetch_brc_auto_list),
+    ])
+
+
 if __name__ == "__main__":
     start_time = time.time()
-    print("Number of processors: ", mp.cpu_count())
-    pool = mp.Pool(mp.cpu_count())
+    results: Sequence[IOResultE[List[FutureResultE[str]]]] = asyncio.run(gather_lists())
 
-    results: List[Result[Iterable[Car], str]] = []
-
-    [
-        pool.apply_async(fn, callback=lambda r: results.append(r))
-        for fn in [fetch_moller_auto_list, fetch_inchcape_list, fetch_brc_auto_list]
-    ]
-
-    pool.close()
-    pool.join()
-
-    res: Iterable[Car] = chain(*Fold.collect(results, Success.from_value(())).value_or(()))
-
-
-    car_details = asyncio.run(aaa(res))
-    print(car_details)
-
-    print(*res, sep="\n")
+    print(*results, sep="\n")
     print("--- %s seconds ---" % (time.time() - start_time))
---
+++ utils/sync_to_async.py
@@ -0,0 +1,19 @@
+import asyncio
+import functools
+from typing import TypeVar, Tuple, Dict, Callable, Coroutine, Any
+
+from mypy_extensions import VarArg, KwArg
+
+T = TypeVar('T')
+T1 = TypeVar('T1')
+R = TypeVar('R')
+
+
+def sync_to_async(fn: Callable[..., R]) -> Callable[[VarArg(Tuple[T, ...]), KwArg(Dict[str, T1])], Coroutine[Any, Any, R]]:
+    @functools.wraps(fn)
+    async def wrapper(*args: Tuple[T, ...], **kwargs: Dict[str, T1]) -> R:
+        loop = asyncio.get_event_loop()
+        p_func = functools.partial(fn, *args, **kwargs)
+        return await loop.run_in_executor(None, p_func)
+
+    return wrapper
