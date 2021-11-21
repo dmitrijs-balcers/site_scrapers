ce6f1dd0255ed9bb8037ce09368c660928643180
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2021-11-21 19:02:09 +0200

    Get car text but fail some due to lack of batching

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import asyncio
 import time
 from itertools import chain
-from typing import List, Iterable
+from typing import List, Iterable, Sequence
 
+import httpx
+from returns.future import future_safe, FutureResultE
 from returns.iterables import Fold
 from returns.result import Result, Success
 
@@ -11,6 +14,22 @@ from scrapers.mollerAuto import fetch_moller_auto_list
 from scrapers.inchcape import fetch_inchcape_list
 
 import multiprocessing as mp
+
+
+@future_safe
+async def fetch_car_html(url: str) -> str:
+    async with httpx.AsyncClient(timeout=5) as client:
+        response = await client.get(url)
+        response.raise_for_status()
+        return response.text
+
+
+async def fetch_cars(car: Iterable[Car]) -> Sequence[FutureResultE[str]]:
+    futures: Sequence[FutureResultE[str]] = await asyncio.gather(
+        *[fetch_car_html(car.url) for car in car]
+    )
+
+    return futures
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -28,5 +47,9 @@ if __name__ == "__main__":
     pool.join()
 
     res: Iterable[Car] = chain(*Fold.collect(results, Success.from_value(())).value_or(()))
+
+    car_details = asyncio.run(fetch_cars(res))
+    print(car_details)
+
     print(*res, sep="\n")
     print("--- %s seconds ---" % (time.time() - start_time))
