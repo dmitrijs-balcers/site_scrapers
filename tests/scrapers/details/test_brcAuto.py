41b4583bb38f0ac824b250168f154e9ffa0aeba8
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2021-12-12 17:43:46 +0200

    Fetch good url for brc cars

--- tests/scrapers/details/test_brcAuto.py
+++ tests/scrapers/details/test_brcAuto.py
@@ -11,6 +11,29 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
 class UtilsTestCase(unittest.TestCase):
+    def test_mercedes_gl_30(self) -> None:
+        with open(os.path.join(THIS_FOLDER, "brc_data/brc_mercedes_gl-3.0.html")) as car:
+            self.assertEqual(Success(CarFull(
+                url='https://lv.brcauto.eu/automobilis/c835690-mercedes-benz-gl-3.0l-automatiska',
+                previewImgSrc='https://media.brcauto.eu/cars/all/835690_1.jpg',
+                summary='Mercedes-Benz GL 350',
+                date=CarDate(month='00', year='2013'), type='Apvidus',
+                transmission='Automātiskā',
+                hp='190 Kw (258 HP)', price=33500, vin='WDC1668241A317435',
+                registrationNo=None,
+                mileage=220483,
+                engineSize=None,
+                techInspDate=None,
+                fuelType='diesel',
+                body='suv',
+                drivetrain='awd',
+                color=None,
+                hasWarranty=True,
+                doors='4',
+                country='lv',
+                dealer='brc-auto'
+            )), scrape_brc_auto_car_detail(car.read()))
+
     def test_mercedes_ml_500(self) -> None:
         with open(os.path.join(THIS_FOLDER, "brc_data/brc_mercedes_ml_500.html")) as car:
             self.assertEqual(Success(CarFull(
