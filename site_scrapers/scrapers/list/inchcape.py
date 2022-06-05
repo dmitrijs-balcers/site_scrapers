6bd24bbe93b3276841ef0e0487c0835636b78e01
Dmitrijs Balcers <dmitrijs.balcers@gmail.com>
2022-06-05 13:31:34 +0300

    Adjust inchcepe scraper to new design

--- site_scrapers/scrapers/list/inchcape.py
+++ site_scrapers/scrapers/list/inchcape.py
@@ -2,24 +2,25 @@ from typing import Iterable
 
 from gazpacho import Soup
 from returns.pipeline import flow
+from returns.pointfree import bind
+
 from site_scrapers.scrapers.utils import find_one, find_many
 
 
 def fetch_inchcape_urls(page: int = 1) -> Iterable[str]:
     print("fetch_inchcape_urls")
     soup = Soup.get(
-        url=f"https://certified.inchcape.lv/auto-ajax?drive=AWD&catalog_page={page}&_=1637248077760",
+        url=f"https://certified.inchcape.lv/auto-ajax?&catalog_page={page}",
     )
 
     return flow(
         soup,
-        find_many("div", {"class": "offer__info-row"}),
-        lambda _: _.bind(lambda _: _),
-        lambda _: [flow(
+        find_many("article", {"class": "products__item js-product-item"}),
+        lambda _: _.map(lambda _: [flow(
             car,
             find_one("a"),
-            lambda anchor: anchor.map(lambda _: _.attrs),
-            lambda _: _.bind(lambda attrs: attrs["href"]),
-            lambda _: f"https://certified.inchcape.lv/{_}"
-        ) for car in _],
+            lambda _: _.map(lambda _: f"https://certified.inchcape.lv/{_.attrs['href']}"),
+            lambda _: _.bind(lambda _: _)
+        ) for car in _]),
+        bind(lambda _: _)
     )
